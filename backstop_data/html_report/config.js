837c8aa0ad6668ccd95e1b35b33c5d30ae200b4c
Alex Verbruggen <verbruggenalex@gmail.com>
2018-05-22 13:53:17 -0700

    Backstop no docksal

--- backstop_data/html_report/config.js
+++ backstop_data/html_report/config.js
@@ -4,20 +4,21 @@ report({
     {
       "pair": {
         "reference": "../bitmaps_reference/backstop_default_BackstopJS_Homepage_0_document_0_phone.png",
-        "test": "../bitmaps_test/20180522-203604/backstop_default_BackstopJS_Homepage_0_document_0_phone.png",
+        "test": "../bitmaps_test/20180522-205256/backstop_default_BackstopJS_Homepage_0_document_0_phone.png",
         "selector": "document",
         "fileName": "backstop_default_BackstopJS_Homepage_0_document_0_phone.png",
         "label": "BackstopJS Homepage",
         "requireSameDimensions": true,
         "misMatchThreshold": 0.1,
+        "url": "http://localhost",
+        "referenceUrl": "",
         "diff": {
           "isSameDimensions": true,
           "dimensionDifference": {
             "width": 0,
             "height": 0
           },
-          "misMatchPercentage": "0.00",
-          "getDiffImage": null
+          "misMatchPercentage": "0.00"
         }
       },
       "status": "pass"
@@ -25,23 +26,25 @@ report({
     {
       "pair": {
         "reference": "../bitmaps_reference/backstop_default_BackstopJS_Homepage_0_document_1_tablet.png",
-        "test": "../bitmaps_test/20180522-203604/backstop_default_BackstopJS_Homepage_0_document_1_tablet.png",
+        "test": "../bitmaps_test/20180522-205256/backstop_default_BackstopJS_Homepage_0_document_1_tablet.png",
         "selector": "document",
         "fileName": "backstop_default_BackstopJS_Homepage_0_document_1_tablet.png",
         "label": "BackstopJS Homepage",
         "requireSameDimensions": true,
         "misMatchThreshold": 0.1,
+        "url": "http://localhost",
+        "referenceUrl": "",
         "diff": {
           "isSameDimensions": true,
           "dimensionDifference": {
             "width": 0,
             "height": 0
           },
-          "misMatchPercentage": "0.00",
-          "getDiffImage": null
+          "misMatchPercentage": "0.00"
         }
       },
       "status": "pass"
     }
-  ]
+  ],
+  "id": "backstop_default"
 });
