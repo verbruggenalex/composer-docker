e508c725f0aae933b4c78f0e1080e5e9698c3bc8
Alex Verbruggen <verbruggenalex@gmail.com>
2018-05-22 13:59:51 -0700

    Good.

--- backstop_data/html_report/config.js
+++ backstop_data/html_report/config.js
@@ -4,13 +4,13 @@ report({
     {
       "pair": {
         "reference": "../bitmaps_reference/backstop_default_BackstopJS_Homepage_0_document_0_phone.png",
-        "test": "../bitmaps_test/20180522-205256/backstop_default_BackstopJS_Homepage_0_document_0_phone.png",
+        "test": "../bitmaps_test/20180522-205938/backstop_default_BackstopJS_Homepage_0_document_0_phone.png",
         "selector": "document",
         "fileName": "backstop_default_BackstopJS_Homepage_0_document_0_phone.png",
         "label": "BackstopJS Homepage",
         "requireSameDimensions": true,
         "misMatchThreshold": 0.1,
-        "url": "http://localhost",
+        "url": "http://ec.europa.eu",
         "referenceUrl": "",
         "diff": {
           "isSameDimensions": true,
@@ -26,13 +26,13 @@ report({
     {
       "pair": {
         "reference": "../bitmaps_reference/backstop_default_BackstopJS_Homepage_0_document_1_tablet.png",
-        "test": "../bitmaps_test/20180522-205256/backstop_default_BackstopJS_Homepage_0_document_1_tablet.png",
+        "test": "../bitmaps_test/20180522-205938/backstop_default_BackstopJS_Homepage_0_document_1_tablet.png",
         "selector": "document",
         "fileName": "backstop_default_BackstopJS_Homepage_0_document_1_tablet.png",
         "label": "BackstopJS Homepage",
         "requireSameDimensions": true,
         "misMatchThreshold": 0.1,
-        "url": "http://localhost",
+        "url": "http://ec.europa.eu",
         "referenceUrl": "",
         "diff": {
           "isSameDimensions": true,
